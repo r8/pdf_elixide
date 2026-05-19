f74b5af6363734e6f16d6f01d2b3a3055fb7e0ac
Sergey Storchay <r8@r8.com.ua>
2026-05-19 12:40:51 +0300

    refactor: replace repeated error handling with a private function

--- lib/pdf_elixide/native.ex
+++ lib/pdf_elixide/native.ex
@@ -6,9 +6,11 @@ defmodule PdfElixide.Native do
     crate: :pdf_elixide_nif
 
   # Document operations
-  def document_open(_path), do: :erlang.nif_error(:nif_not_loaded)
-  def document_from_bytes(_bytes), do: :erlang.nif_error(:nif_not_loaded)
-  def document_page_count(_doc), do: :erlang.nif_error(:nif_not_loaded)
-  def document_version(_doc), do: :erlang.nif_error(:nif_not_loaded)
-  def document_extract_text(_doc, _page_index), do: :erlang.nif_error(:nif_not_loaded)
+  def document_open(_path), do: err()
+  def document_from_bytes(_bytes), do: err()
+  def document_page_count(_doc), do: err()
+  def document_version(_doc), do: err()
+  def document_extract_text(_doc, _page_index), do: err()
+
+  defp err, do: :erlang.nif_error(:nif_not_loaded)
 end
