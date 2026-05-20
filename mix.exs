8bc6e25c3ecf4353984a9fa56dc77f76332d5d4e
Sergey Storchay <r8@r8.com.ua>
2026-05-20 18:40:01 +0300

    chore: update Elixir version requirement to 1.17

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PdfElixide.MixProject do
     [
       app: :pdf_elixide,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
